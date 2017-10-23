d3ee518a5e50b244d5f023c95a36db7e3ced1450
Tejabhishek <tejabhishek@gmail.com>
2017-10-22 19:29:41 -0700

    require notes.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-console.log('Starting App..');
+console.log('Starting App.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
 
-fs.appendFileSync('greetings.txt','Hello world!');
+//fs.appendFileSync('greetings.txt','Hello world!');
 var user = os.userInfo();
 
 //console.log(dem.uid);
