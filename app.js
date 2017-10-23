c82c187e3b184a874eba1e16d4969ae38ada8b2c
Tejabhishek <tejabhishek@gmail.com>
2017-10-22 19:15:16 -0700

    Hello World with Node!

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('Starting App..');
+
+const fs = require('fs');
+const os = require('os');
+
+
+fs.appendFileSync('greetings.txt','Hello world!');
+var dem = os.userInfo();
+
+console.log(dem.uid);
+fs.appendFileSync('greetings.txt','Hello world!'+dem.username);
